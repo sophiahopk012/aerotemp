36173ae2efe69ca4f6cffeea415587e88f8ed849
Ayushi Dosieah <dosieahayushi@gmail.com>
2025-01-18 21:45:12 -0500

    sophia python api

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """
 from openai import OpenAI
 
-client =OpenAI(api_key=os.environ["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"],)
+
 
 completion = client.completions.create(model='curie')
 print(completion.choices[0].text)
